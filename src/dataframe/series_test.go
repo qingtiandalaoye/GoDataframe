ab788e2ebfabde232e6d4dea55c618e8d3cb10e4
DESKTOP-F4JF6MJ\lbc <3952700@qq.com>
2016-11-13 21:27:20 +0800

    use  Values(s)

--- src/dataframe/series_test.go
+++ src/dataframe/series_test.go
@@ -44,31 +44,31 @@ func TestCreate_named_series(t *testing.T) {
 func TestCreate_named_series_value(t *testing.T) {
 	var se Series= NamedStrings("nameABC", []string{"A", "B", "", "1", "2", "true", "false", "123456.01", "78.9", "2016-01-01", "2016-12-31"})
 	fmt.Printf("%s  type=%s  length=%d \n", se, se.Type(), Len(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
 
 	se = NamedInts("name1", []int{123456, 678, 789})
 	fmt.Printf("%s  type=%s  length=%d \n", se, se.Type(), Len(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
 
 	se = NamedFloats("name1", []float64{123456.01, 56.7, 78.9})
 	fmt.Printf("%s  type=%s  length=%d \n", se, se.Type(), Len(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
 
 	se = NamedBools("name1",[]bool{true,true,false})
 	fmt.Printf("%s  type=%s  length=%d \n", se, se.Type(), Len(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
 
 	se = NamedTimes("name1", "2006-01-01", []string{ "2016-01-01", "2016-12-12", "2016-12-11"})
 	fmt.Printf("%s  type=%s  length=%d \n", se, se.Type(), Len(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
 }
@@ -76,10 +76,10 @@ func TestCreate_named_series_value(t *testing.T) {
 func TestSet_Index_series(t *testing.T) {
 	var se Series = NamedStrings("nameABC", []string{"A", "B", "", "1", "2", "true", "false", "123456.01", "78.9", "2016-01-01", "2016-12-31"})
 	fmt.Printf("%s  string of Series is: %s\n", se, String(se))
-	for i, v := range se.values {
+	for i, v := range Values(se) {
 		fmt.Printf("%d, %s\n", i, v )
 	}
-	val := se.values
+	val := Values(se)
 	myse := setIndex(&se, &val)
 	se = *myse
 
